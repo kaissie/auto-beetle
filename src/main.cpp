58917ab7f6240a3240af23468accaeb5e3565d38
Kaisei Wassie <57220447+kaissie@users.noreply.github.com>
2020-01-13 23:57:02 +0900

    add cui-interface

--- src/main.cpp
+++ src/main.cpp
@@ -2,6 +2,81 @@
 #include <string>
 #include "dfa.hpp"
 
+void interface(){
+  std::set<char> state_set;
+  char init_state;
+  std::set<char> accept_state_set;
+  std::set<char> alphabet_set;
+  std::unordered_map<char, std::unordered_map<char, char>> transitions;
+
+  std::cout << "Enter states ( '}' is end )" << '\n';
+  char inchar;
+  std::cout << "state_set = {" << "\n";
+  while(true){
+    std::cin >> inchar;
+    if (inchar == '}')
+      break;
+    state_set.insert(inchar);
+  }
+  for(auto s: state_set)
+    std::cout << s << ", ";
+  std::cout << '\n';
+
+  std::cout << "Enter the init state ( '}' is end )" << '\n';
+  std::cout << "init_state = ";
+  std::cin >> init_state;
+
+  std::cout << "Enter accept states ( '}' is end )" << '\n';
+  std::cout << "accept_state_set = {" << "\n";
+  while(true){
+    std::cin >> inchar;
+    if (inchar == '}')
+      break;
+    accept_state_set.insert(inchar);
+  }
+  std::cout << "Enter alphabet ( '}' is end )" << '\n';
+  std::cout << "alphabet_set = {" << "\n";
+  while(true){
+    std::cin >> inchar;
+    if (inchar == '}')
+      break;
+    alphabet_set.insert(inchar);
+  }
+  for(auto s: alphabet_set)
+    std::cout << s << ", ";
+  std::cout << '\n';
+
+  std::cout << "Enter transition table ( '}' is end )" << '\n';
+  std::cout << "transitions = {" << "\n";
+  for(auto s: state_set) {
+    std::unordered_map<char, char> to_state;
+    for(auto c: alphabet_set) {
+      std::cout << s << " -"<< c << "-> ";
+      std::cin >> inchar;
+      to_state.emplace(c,inchar);
+    }
+    transitions.emplace(s,to_state);
+  }
+  std::cout << "Transition Function" << '\n';
+  for(auto s: transitions) {
+    for(auto c: transitions.at(s.first)) {
+      std::cout << s.first << " -"<< c.first << "-> ";
+      std::cout << c.second << '\n';
+    }
+  }
+
+
+  DFA<char> dfa{state_set,alphabet_set,init_state,accept_state_set,transitions};
+  std::string instr;
+  while(true){
+    std::cout << ">> ";
+    std::cin >> instr;
+    if (instr == "exit")
+      break;
+    std::cout << std::boolalpha <<dfa.run(instr) << '\n';
+  }
+}
+
 int main(int argc, char const *argv[]) {
   std::cout << "Hello, auto-beetle!" << '\n';
   DFA<int> dfa{
@@ -29,6 +104,6 @@ int main(int argc, char const *argv[]) {
       break;
     std::cout << std::boolalpha <<dfa.run(instr) << '\n';
   }
-  std::cout << "s" << '\n';
+  interface();
   return 0;
 }
